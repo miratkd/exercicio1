5a525933800dc803f029c10bb314f4e8373267f4
lucas <lucas-mira1@hotmail.com>
2021-01-17 11:11:26 -0300

    Project created

---
+++ exercicioProposto/src/main/java/com/LMira/exercicioProposto/ExercicioPropostoApplication.java
@@ -0,0 +1,13 @@
+package com.LMira.exercicioProposto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExercicioPropostoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExercicioPropostoApplication.class, args);
+	}
+
+}
---
+++ exercicioProposto/src/test/java/com/LMira/exercicioProposto/ExercicioPropostoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LMira.exercicioProposto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExercicioPropostoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
