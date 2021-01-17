0a7fe434bade226cc0e37bbe3d41ab75759dc162
lucas <lucas-mira1@hotmail.com>
2021-01-17 15:27:15 -0300

    FindById created

--- exercicioProposto/src/main/java/com/LMira/exercicioProposto/servicies/ClientService.java
+++ exercicioProposto/src/main/java/com/LMira/exercicioProposto/servicies/ClientService.java
@@ -1,5 +1,7 @@
 package com.LMira.exercicioProposto.servicies;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -9,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.LMira.exercicioProposto.dto.ClientDTO;
 import com.LMira.exercicioProposto.entities.Client;
 import com.LMira.exercicioProposto.repositories.ClientRepository;
+
 
 @Service
 public class ClientService {
@@ -20,6 +23,13 @@ public class ClientService {
 	public Page<ClientDTO> findAllPaged(PageRequest pageRequest) {
 		Page<Client> list = repository.findAll(pageRequest);
 		return list.map(x -> new ClientDTO(x));
+	}
+	
+	@Transactional(readOnly = true)
+	public ClientDTO findById(Long id) {
+		Optional<Client> obj = repository.findById(id);
+		Client entity = obj.get();
+		return new ClientDTO(entity);
 	}
 
 }
