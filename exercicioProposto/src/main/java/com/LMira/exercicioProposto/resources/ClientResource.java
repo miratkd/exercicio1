4f7296ac844133b60c1ba8373a3c4bc74072c8a8
lucas <lucas-mira1@hotmail.com>
2021-01-17 15:21:39 -0300

    FindAll created

--- exercicioProposto/src/main/java/com/LMira/exercicioProposto/resources/ClientResource.java
+++ exercicioProposto/src/main/java/com/LMira/exercicioProposto/resources/ClientResource.java
@@ -1,18 +1,35 @@
 package com.LMira.exercicioProposto.resources;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.LMira.exercicioProposto.dto.ClientDTO;
 import com.LMira.exercicioProposto.servicies.ClientService;
 
 @RestController
-@RequestMapping(value = "/client")
+@RequestMapping(value = "/clients")
 public class ClientResource {
 	
 	@Autowired
 	private ClientService service;
 	
-	
+	@GetMapping
+	public ResponseEntity<Page<ClientDTO>> findAll(
+			@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "10") Integer linesPerPage,
+			@RequestParam(value = "orderBy", defaultValue = "name") String orderBy,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction
+			){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Page<ClientDTO> list = service.findAllPaged(pageRequest);
+		return ResponseEntity.ok().body(list);
+	}
 
 }
--- exercicioProposto/src/main/java/com/LMira/exercicioProposto/servicies/ClientService.java
+++ exercicioProposto/src/main/java/com/LMira/exercicioProposto/servicies/ClientService.java
@@ -1,8 +1,13 @@
 package com.LMira.exercicioProposto.servicies;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.LMira.exercicioProposto.dto.ClientDTO;
+import com.LMira.exercicioProposto.entities.Client;
 import com.LMira.exercicioProposto.repositories.ClientRepository;
 
 @Service
@@ -10,5 +15,11 @@ public class ClientService {
 	
 	@Autowired
 	private ClientRepository repository;
+
+	@Transactional(readOnly = true)
+	public Page<ClientDTO> findAllPaged(PageRequest pageRequest) {
+		Page<Client> list = repository.findAll(pageRequest);
+		return list.map(x -> new ClientDTO(x));
+	}
 
 }
